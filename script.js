074cac5a48526bb2f01918188f9bb2829b663bf0
jammy289 <159913049+jammy289@users.noreply.github.com>
2024-02-13 12:49:39 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const yesBtn = document.getElementById("yes")
+
+const HK1 = document.getElementById("HK1")
+
+const HK2 = document.getElementById("HK2")
+
+const willText = document.getElementById("will")
+
+const yayText = document.getElementById("yay")
+
+yesBtn.addEventListener("click", () => {
+
+    HK1.style.display = "none";
+
+    HK2.style.display = "block";
+
+    willText.style.display = "none";
+
+    yayText.style.display = "block";
+
+    
+
+})
